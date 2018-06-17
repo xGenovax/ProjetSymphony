2d78c0a7a4fdbae2e37f19320db6967f0672c671
Paul <angelsuikoden@gmail.com>
2018-06-17 19:06:44 +0200

    Ajout table

--- README.md
+++ README.md
@@ -6,3 +6,26 @@ A Symfony project created on May 29, 2018, 9:44 am.
 
 Git install:
 http://msysgit.github.io
+
+Travaillez sur votre propre branche en la "tirant" de la branche develop
+git checkout -b origin develop
+
+Pour pusher (uploader) vos modifications sur votre branche
+git gui
+
+Dans la fenetre en haut a gauche vous selectionnez les modifications a "commit" (enregistrer localement) qui se retrouve en bas a gauche
+Pour commit on met un message commentaire et on clic sur commit
+Puis si on veut uploader ses "commit" on clic sur Push
+
+
+Demarrer le serveur:
+php bin/console server:run
+
+
+Generons la table correspondante dans la base :
+
+php bin/console doctrine:schema:update --dump-sql
+
+apres
+
+php bin/console doctrine:schema:update --force
--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -8,6 +8,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use AppBundle\Entity\Questionnaire;
 use AppBundle\Form\Type\QuestionnaireType;
 
@@ -37,10 +38,10 @@ class DefaultController extends Controller
 
       ]);
     }
-/**
-*QUESTIONNAIRES
-*
-*/
+    /**
+    *QUESTIONNAIRES
+    *
+    */
     public function consulterQuestionnairesAction(){
       $repository = $this->getDoctrine()->getRepository(Questionnaire::class);
       $products = $repository->findAll();
@@ -49,12 +50,33 @@ class DefaultController extends Controller
         'products' => $products
       ]);
     }
+    /**
+     * Finds and displays a questionnaire entity.
+     *
+     * @Route("/{id}", name="questionnaire_show")
+     * @Method("GET")
+     */
+    public function voirQuestionnaireAction(Questionnaire $questionnaire)
+    {
+        $deleteForm = $this->createDeleteForm($questionnaire);
+
+        return $this->render('default/questionnaire_voir.html.twig', array(
+            'questionnaire' => $questionnaire,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
     public function ajouterQuestionnaireAction(Request $request, $id) {
             $entityManager = $this->getDoctrine()->getManager();
             $questionnaire=$entityManager->getRepository('AppBundle:Questionnaire')->find($id) ;
-            $form = $this->createForm(QuestionnaireType::class, $questionnaire,
-                       array('action' => $this->generateUrl('questionnaire_ajouter',
-                                            array('id' => $questionnaire->getId())) ));
+            if($questionnaire == null){
+              $questionnaire = new Questionnaire;
+              $form = $this->createForm(QuestionnaireType::class, $questionnaire);
+            }else{
+              $form = $this->createForm(QuestionnaireType::class, $questionnaire,
+                         array('action' => $this->generateUrl('questionnaire_ajouter',
+                                              array('id' => $questionnaire->getId())) ));
+            }
+
             $form->add('submit', SubmitType::class, array('label' => 'Modifier'));
             $form->handleRequest($request);
             if ($form->isSubmitted() && $form->isValid()) {
@@ -63,7 +85,7 @@ class DefaultController extends Controller
                     $entityManager->persist($questionnaire);
                     $entityManager->flush();
                     $url = $this->generateUrl('questionnaires',
-                                               array('id' => $questionnaire->getId()));
+                                               array());
                     return $this->redirect($url);
             }
             return $this->render('default/questionnaire_edit.html.twig',
@@ -96,5 +118,40 @@ class DefaultController extends Controller
       return $this->render('default/utilisateur.html.twig', [
 
       ]);
+    }
+
+    /**
+     * Deletes a questionnaire entity.
+     *
+     * @Route("/{id}", name="questionnaire_supprimer")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, Questionnaire $questionnaire)
+    {
+        $form = $this->createDeleteForm($questionnaire);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($questionnaire);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('questionnaires');
+    }
+    /**
+     * Creates a form to delete a questionnaire entity.
+     *
+     * @param Questionnaire $questionnaire The questionnaire entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Questionnaire $questionnaire)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('questionnaire_supprimer', array('id' => $questionnaire->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
     }
 }
